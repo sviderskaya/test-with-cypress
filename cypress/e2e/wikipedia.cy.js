2e62ad4e7f2c09ad6281cfb86348a47e56c3c5ed
Juliia Sviderska <swiderskya111@gmail.com>
2024-03-14 19:03:35 +0100

    added first cypress test

---
+++ cypress/e2e/wikipedia.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://wikipedia.org')
+    cy.get('#js-link-box-en').click()
+    cy.get('#pt-createaccount-2').should('be.visible')
+    cy.get('#pt-login-2').should('be.visible')
+    cy.get('#vector-user-links-dropdown-checkbox').should('exist')
+  })
+})
